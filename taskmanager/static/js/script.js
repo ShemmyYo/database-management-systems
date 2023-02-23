b24aa7762438069dbc8ad0efc5c450038dc039d8
ShemmyYo <rabiliamemo@gmail.com>
2023-02-23 21:19:16 +0000

    modal for Delete function

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    let modalEl = document.querySelectorAll('.modal');
+    M.Modal.init(modalEl);
+  });
